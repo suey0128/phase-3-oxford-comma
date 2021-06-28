df471c1432dc359854c13633848b50d96d9d115d
suey0128 <syui0128@gmail.com>
2021-06-27 20:20:50 -0700

    commit tree 3b0389f3401ff8cbe83606abe5f5d34a131c4e26

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    # return array.join(" and ") if array.size < 3
+    # array[-1] = "and" + array[-1]
+    # array.join(",")
+    if array.size < 3 
+        array.join(" and ")
+    else 
+        array[-1] = "and " + array[-1]
+        array.join(", ")
+    end
 end
